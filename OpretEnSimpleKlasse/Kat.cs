72f4c954018514f7f800cc0d16398343d186b446
Alexander Runge <alexander@khrunge.dk>
2022-11-30 08:24:29 +0100

    Add project files.

---
+++ OpretEnSimpleKlasse/Kat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpretEnSimpleKlasse
+{
+    internal class Kat
+    {
+        public string? Farve { get; set; }
+        public void SkiftFarve()
+        {
+            Farve = "gul";
+        }
+    }
+}
---
+++ OpretEnSimpleKlasse/Program.cs
@@ -0,0 +1,7 @@
+﻿using OpretEnSimpleKlasse;
+
+Kat kat = new Kat();
+kat.Farve = "grå";
+Console.WriteLine(kat.Farve);
+kat.SkiftFarve();
+Console.WriteLine(kat.Farve);
